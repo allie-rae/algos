c968fb53b89fc9562ed3dbb732e73ac6a86ba73c
Allie Robinson <allierob93@gmail.com>
2024-11-21 23:09:08 -0800

    :sparkles: Validate subsequence

--- algos.js
+++ algos.js
@@ -14,3 +14,27 @@ function twoNumberSum(array, targetSum) {
 
   return [];
 }
+
+// Validate Subsequence - Return true if the sequence is a subsequence of the array
+function isValidSubsequence(array, sequence) {
+  let arrayPointer = 0;
+  let subPointer = 0;
+
+  while (
+    arrayPointer <= array.length - 1 ||
+    subPointer <= subPointer.length - 1
+  ) {
+    if (array[arrayPointer] === sequence[subPointer]) {
+      arrayPointer++;
+      subPointer++;
+    } else {
+      arrayPointer++;
+    }
+  }
+
+  if (subPointer === sequence.length) {
+    return true;
+  }
+
+  return false;
+}
