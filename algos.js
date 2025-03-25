73400080a0efa747ae95897e887ec68e5a51b6a5
Allie Robinson <allierob93@gmail.com>
2025-03-25 00:41:19 -0700

    :art: Sorted squared array

--- algos.js
+++ algos.js
@@ -38,3 +38,32 @@ function isValidSubsequence(array, sequence) {
 
   return false;
 }
+
+// Sorted Squared Array - Return a new array of the squared values of the input array
+// Solution 1
+function sortedSquaredArray(array) {
+  const squaredArray = [];
+  for (let i = 0; i < array.length; i++) {
+    squaredArray.push(array[i] * array[i]);
+  }
+  squaredArray.sort((a, b) => a - b);
+  return squaredArray;
+}
+
+// Solution 2
+function sortedSquaredArray(array) {
+  let startPointer = 0;
+  let endPointer = array.length - 1;
+  const squaredArray = [];
+
+  while (startPointer <= endPointer) {
+    if (Math.abs(array[startPointer]) > Math.abs(array[endPointer])) {
+      squaredArray.unshift(array[startPointer] * array[startPointer]);
+      startPointer++;
+    } else {
+      squaredArray.unshift(array[endPointer] * array[endPointer]);
+      endPointer--;
+    }
+  }
+  return squaredArray;
+}
